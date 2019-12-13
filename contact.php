5bc48b8018de2f1bc8fcc1c0f7f32fb5f0a830a0
momoipswebtech <jeronimo.gonzalez@ipswebtech.com>
2019-12-13 16:23:44 -0600

    css de formulario de contacto

--- contact.php
+++ contact.php
@@ -5,6 +5,12 @@ $para = 'jeronimo.gonzalez@live.com.mx';
 $asunto = 'Mensaje desde cgdeportes.com';
 
 $mailheader = "From: ".$POST["email"]."\r\n";
+$mailheader = "Reply-To: ".$POST["email"]."\r\n";
+$mailheader = "Content-type: text/html; charset=utf-8 \r\n";
+
+$MESSAGE_BODY = "Nombre: ".$_POST["name"]."\n";
+$MESSAGE_BODY = "\n<br>Email: ".$_POST["email"]."\n";
+$MESSAGE_BODY = "\n<br>Mensaje: ".nl2br($_POST["message"])."\n";
 
 mail($para, $asunto, $MESSAGE_BODY, $mailheader) or die("Error al enviar email");
 
